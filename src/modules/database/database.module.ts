6ff866f66821cba1b19a2de264db92255cf27ddc
seonggukchoi <seonggukchoi.kr@gmail.com>
2020-07-03 23:51:04 +0900

    Remove unecessary array

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,8 +1,9 @@
 import { Module, Global } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
-import { entities } from './entities';
+import * as Entities from './entities';
 
+const entities = Object.values(Entities);
 const databaseConfig = <TypeOrmModuleOptions>{
   ...config.get<TypeOrmModuleOptions>('database'),
   entities,
--- src/modules/database/entities/index.ts
+++ src/modules/database/entities/index.ts
@@ -2,6 +2,4 @@ import { ApplicationEntity } from './application.entity';
 import { AuthorizationEntity } from './authorization.entity';
 import { UserEntity } from './user.entity';
 
-const entities = [ApplicationEntity, AuthorizationEntity, UserEntity];
-
-export { entities, ApplicationEntity, AuthorizationEntity, UserEntity };
+export { ApplicationEntity, AuthorizationEntity, UserEntity };
